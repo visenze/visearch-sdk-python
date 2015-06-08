4f0d77f35ef29ee4a0537eb076e3459ad951b8f8
Han Xiaogang <xganghan@gmail.com>
2015-06-08 13:46:57 +0800

    Release 0.2.2

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ test_requirements = [
 
 setup(
     name='visearch',
-    version='0.2.0',
+    version='0.2.2',
     description="ViSearch Python SDK",
     long_description=readme + '\n\n' + history,
     author="Han Xiaogang",
